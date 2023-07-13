31d81e4be876918bde598c8b08284784384a43bd
jaytality <johnathan.tiong@gmail.com>
2023-07-13 14:48:10 +1000

    added style import

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from './styles/app.css';
+
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
